ccbe1adbb1e4f40c9390adcddf30d1bf4e1b211a
biggame27 <koharrisonko@gmail.com>
2024-09-28 11:54:31 -0500

    added links

---
+++ app/(root)/current/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Current = () => {
+  return (
+    <div>
+      current
+    </div>
+  )
+}
+
+export default Current;
---
+++ app/(root)/gpa-calculator/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GPACalc = () => {
+  return (
+    <div>
+      gpa calc
+    </div>
+  )
+}
+
+export default GPACalc
