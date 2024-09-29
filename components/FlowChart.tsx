29f4508b1ade5af67d49f5d8be81026856340e44
biggame27 <koharrisonko@gmail.com>
2024-09-29 02:45:34 -0500

    added colors to flow

--- components/FlowChart.tsx
+++ components/FlowChart.tsx
@@ -3,16 +3,39 @@ import React from 'react'
 import { ReactFlow, Handle, Position, Edge, MarkerType } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
-
-
-// interface Node {
-//   id: string;
-//   position: { x: number, y: number };
-//   data: { label: string };
-// }
+// make a color component, 
+let visited = [false];
 const CustomNode = ({ data }: any) => {  //eslint-disable-line @typescript-eslint/no-explicit-any
+  // data.realCourses
+  const cor = data.label;
+  let green = false;
+  let yellow = false;
+  for (let i = 0; i < data.realCourses.length; i++) {
+    if (visited[i] === true)
+      continue;
+    if (data.realCourses[i].elective === true && data.label.substring(0,3) === "EAE") {
+      // console.log(data.realCourses[i])
+      visited[i] = true;
+      if (data.realCourses[i].planned === true) yellow = true;
+      else green = true;
+    }
+    if (data.realCourses[i].ucc === true && data.label.substring(0,3) === "UCC") {
+      visited[i] = true;
+      if (data.realCourses[i].planned === true) yellow = true;
+      else green = true;
+    }
+    if (green || yellow)
+      break;
+    if (cor.substring(0,data.realCourses[i].department.length) == data.realCourses[i].department && cor.substring(data.realCourses[i].department.length+1, data.realCourses[i].department.length+4) == String(data.realCourses[i].code))
+    {
+      visited[i] = true;
+      if (data.realCourses[i].planned === true) yellow = true;
+      else green = true;
+    }
+  }
+  // console.log(EAEs)
   return (
-    <div style={{ padding: '10px', border: '1px solid #777', borderRadius: '5px', background: '#fff', position: 'relative', width:148}} className="text-center">
+    <div style={{ padding: '10px', border: '1px solid #777', borderRadius: '5px', background: `${green ? `#AAFF00` : (yellow ? `#FFEA00` : ``)}`, position: 'relative', width:148}} className={`text-center`}>
       <Handle
         type="target"
         position={Position.Left} // Incoming connection point on the left
@@ -65,56 +88,57 @@ const CustomNode = ({ data }: any) => {  //eslint-disable-line @typescript-eslin
     </div>
   );
 };
-
-const FlowChart = () => {
+// change science, general elective, emphasis elective, cs elective
+const FlowChart = ({courses} : {courses:any}) => {
   // const [nodes, setNodes] = useState<Node[]>([
+  const realCourses = courses;
+  visited = new Array(realCourses.length); // Creates an empty array with a length of 5
+  visited.fill(false);
+
   const nodes = [
-    { id: '1', position: { x: 350, y: 350 }, data: { label: 'ENGR 102 (2)' }, type: 'customNode' },
-    { id: '2', position: { x: 350, y: 450 }, data: { label: 'MATH 151 (4)' }, type: 'customNode' },
-    { id: '3', position: { x: 350, y: 535 }, data: { label: 'CHEM 107+117 (4)' }, type: 'customNode' },
-    { id: '4', position: { x: 350, y: 650 }, data: { label: 'ENGL 104 (3)' }, type: 'customNode' },
-    { id: '5', position: { x: 350, y: 750 }, data: { label: 'University Core Curriculum (3)' }, type: 'customNode' },
-    { id: '6', position: { x: 650, y: 350 }, data: { label: 'ENGR 216 (2)' }, type: 'customNode'},
-    { id: '7', position: { x: 650, y: 450 }, data: { label: 'MATH 152 (4)' }, type: 'customNode' },
-    { id: '8', position: { x: 650, y: 520 }, data: { label: 'PHYS 206 (3)' }, type: 'customNode' },
-    { id: '9', position: { x: 650, y: 650 }, data: { label: 'ENGL 210 (3) OR COMM 203 (3) OR COMM 205 (3)' }, type: 'customNode' },
-    { id: '10', position: { x: 650, y: 850 }, data: { label: 'University Core Curriculum (3)' }, type: 'customNode' },
-    { id: '11', position: { x: 950, y: 350 }, data: { label: 'CSCE 121 (4)' }, type: 'customNode' },
-    { id: '12', position: { x: 950, y: 450 }, data: { label: 'CSCE 222 (3)' }, type: 'customNode' },
-    { id: '13', position: { x: 950, y: 550 }, data: { label: 'MATH 304 (3)' }, type: 'customNode' },
-    { id: '14', position: { x: 950, y: 650 }, data: { label: 'CSCE 181 (1)' }, type: 'customNode' },
-    { id: '15', position: { x: 950, y: 750 }, data: { label: 'Science Elective (4)' }, type: 'customNode' },
-    { id: '16', position: { x: 950, y: 850 }, data: { label: 'General Elective (1)' }, type: 'customNode' },
-    { id: '17', position: { x: 1250, y: 350 }, data: { label: 'CSCE 221 (4) ' }, type: 'customNode' },
-    { id: '18', position: { x: 1250, y: 450 }, data: { label: 'CSCE 312 (4)' }, type: 'customNode' },
-    { id: '19', position: { x: 1250, y: 550 }, data: { label: 'CSCE 314 (3)' }, type: 'customNode' },
-    { id: '20', position: { x: 1250, y: 650 }, data: { label: 'Emphasis Area Elective (3)' }, type: 'customNode' },
-    { id: '21', position: { x: 1250, y: 750 }, data: { label: 'University Core Curriculum (3)' }, type: 'customNode' },
-    { id: '22', position: { x: 1550, y: 350 }, data: { label: 'CSCE 313 (4)' }, type: 'customNode' },
-    { id: '23', position: { x: 1550, y: 450 }, data: { label: 'CSCE 315 (3)' }, type: 'customNode' },
-    { id: '24', position: { x: 1550, y: 550 }, data: { label: 'CSCE 481 (1)' }, type: 'customNode' },
-    { id: '25', position: { x: 1550, y: 650 }, data: { label: 'STAT 211 (3)' }, type: 'customNode' },
-    { id: '26', position: { x: 1550, y: 750 }, data: { label: 'Emphasis Area Elective (3)' }, type: 'customNode' },
-    { id: '27', position: { x: 1550, y: 850 }, data: { label: 'University Core Curriculum (3)' }, type: 'customNode' },
-    { id: '28', position: { x: 1850, y: 350 }, data: { label: 'CSCE 411 (3)' }, type: 'customNode' },
-    { id: '29', position: { x: 1850, y: 450 }, data: { label: 'CPSC Elective (3)' }, type: 'customNode' },
-    { id: '30', position: { x: 1850, y: 550 }, data: { label: 'CPSC Elective (3)' }, type: 'customNode' },
-    { id: '31', position: { x: 1850, y: 650 }, data: { label: 'MATH 251 (3) OR MATH 302 (3) OR MATH 308 (3)' }, type: 'customNode' },
-    { id: '32', position: { x: 1850, y: 750 }, data: { label: 'Science Elective (3)' }, type: 'customNode' },
-    { id: '33', position: { x: 1850, y: 850 }, data: { label: 'CSCE 399 (0)' }, type: 'customNode' },
-    { id: '34', position: { x: 2150, y: 350 }, data: { label: 'CPSC Elective (3)' }, type: 'customNode' },
-    { id: '35', position: { x: 2150, y: 450 }, data: { label: 'CPSC Elective (3)' }, type: 'customNode' },
-    { id: '36', position: { x: 2150, y: 550 }, data: { label: 'CPSC Elective (3)' }, type: 'customNode' },
-    { id: '37', position: { x: 2150, y: 650 }, data: { label: 'University Core Curriculum (3)' }, type: 'customNode' },
-    { id: '38', position: { x: 2150, y: 750 }, data: { label: 'Emphasis Area Elective (3)' }, type: 'customNode' },
-    { id: '39', position: { x: 2450, y: 350 }, data: { label: 'CSCE 482 (3)' }, type: 'customNode' },
-    { id: '40', position: { x: 2450, y: 450 }, data: { label: 'CPSC Elective (3)' }, type: 'customNode' },
-    { id: '41', position: { x: 2450, y: 550 }, data: { label: 'University Core Curriculum (3)' }, type: 'customNode' },
-    { id: '42', position: { x: 2450, y: 650 }, data: { label: 'University Core Curriculum (3)' }, type: 'customNode' },
-    { id: '43', position: { x: 2450, y: 750 }, data: { label: 'Emphasis Area Elective (3)' }, type: 'customNode' },
-
-    
-
+    { id: '1', position: { x: 150, y: 150 }, data: { label: 'ENGR 102 (2)', realCourses }, type: 'customNode' },
+    { id: '2', position: { x: 150, y: 250 }, data: { label: 'MATH 151 (4)', realCourses }, type: 'customNode' },
+    { id: '3', position: { x: 150, y: 335 }, data: { label: 'CHEM 107+117 (4)', realCourses }, type: 'customNode' },
+    { id: '4', position: { x: 150, y: 450 }, data: { label: 'ENGL 104 (3)', realCourses }, type: 'customNode' },
+    { id: '5', position: { x: 150, y: 550 }, data: { label: 'UCC (3)', realCourses }, type: 'customNode' },
+    { id: '6', position: { x: 450, y: 150 }, data: { label: 'ENGR 216 (2)', realCourses }, type: 'customNode' },
+    { id: '7', position: { x: 450, y: 250 }, data: { label: 'MATH 152 (4)', realCourses }, type: 'customNode' },
+    { id: '8', position: { x: 450, y: 320 }, data: { label: 'PHYS 206 (3)', realCourses }, type: 'customNode' },
+    { id: '9', position: { x: 450, y: 450 }, data: { label: 'ENGL 210 (3)', realCourses }, type: 'customNode' },
+    { id: '10', position: { x: 450, y: 650 }, data: { label: 'UCC (3)', realCourses }, type: 'customNode' },
+    { id: '11', position: { x: 750, y: 150 }, data: { label: 'CSCE 121 (4)', realCourses }, type: 'customNode' },
+    { id: '12', position: { x: 750, y: 250 }, data: { label: 'CSCE 222 (3)', realCourses }, type: 'customNode' },
+    { id: '13', position: { x: 750, y: 350 }, data: { label: 'MATH 304 (3)', realCourses }, type: 'customNode' },
+    { id: '14', position: { x: 750, y: 450 }, data: { label: 'CSCE 181 (1)', realCourses }, type: 'customNode' },
+    { id: '15', position: { x: 750, y: 550 }, data: { label: 'EAE (Science Elective) (4)', realCourses }, type: 'customNode' },
+    { id: '16', position: { x: 750, y: 650 }, data: { label: 'EAE (General Elective) (1)', realCourses }, type: 'customNode' },
+    { id: '17', position: { x: 1050, y: 150 }, data: { label: 'CSCE 221 (4)', realCourses }, type: 'customNode' },
+    { id: '18', position: { x: 1050, y: 250 }, data: { label: 'CSCE 312 (4)', realCourses }, type: 'customNode' },
+    { id: '19', position: { x: 1050, y: 350 }, data: { label: 'CSCE 314 (3)', realCourses }, type: 'customNode' },
+    { id: '20', position: { x: 1050, y: 450 }, data: { label: 'EAE (Emphasis Area Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '21', position: { x: 1050, y: 550 }, data: { label: 'UCC (3)', realCourses }, type: 'customNode' },
+    { id: '22', position: { x: 1250, y: 150 }, data: { label: 'CSCE 313 (4)', realCourses }, type: 'customNode' },
+    { id: '23', position: { x: 1250, y: 250 }, data: { label: 'CSCE 315 (3)', realCourses }, type: 'customNode' },
+    { id: '24', position: { x: 1250, y: 350 }, data: { label: 'CSCE 481 (1)', realCourses }, type: 'customNode' },
+    { id: '25', position: { x: 1250, y: 450 }, data: { label: 'STAT 211 (3)', realCourses }, type: 'customNode' },
+    { id: '26', position: { x: 1250, y: 550 }, data: { label: 'EAE (Emphasis Area Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '27', position: { x: 1250, y: 650 }, data: { label: 'UCC (3)', realCourses }, type: 'customNode' },
+    { id: '28', position: { x: 1450, y: 150 }, data: { label: 'CSCE 411 (3)', realCourses }, type: 'customNode' },
+    { id: '29', position: { x: 1450, y: 250 }, data: { label: 'EAE (CPSC Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '30', position: { x: 1450, y: 350 }, data: { label: 'EAE (CPSC Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '31', position: { x: 1450, y: 450 }, data: { label: 'MATH 251 (3)', realCourses }, type: 'customNode' },
+    { id: '32', position: { x: 1450, y: 550 }, data: { label: 'EAE (Science Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '33', position: { x: 1450, y: 650 }, data: { label: 'CSCE 399 (0)', realCourses }, type: 'customNode' },
+    { id: '34', position: { x: 1650, y: 150 }, data: { label: 'EAE (CPSC Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '35', position: { x: 1650, y: 250 }, data: { label: 'EAE (CPSC Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '36', position: { x: 1650, y: 350 }, data: { label: 'EAE (CPSC Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '37', position: { x: 1650, y: 450 }, data: { label: 'UCC (3)', realCourses }, type: 'customNode' },
+    { id: '38', position: { x: 1650, y: 550 }, data: { label: 'EAE (Emphasis Area Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '39', position: { x: 1850, y: 150 }, data: { label: 'CSCE 482 (3)', realCourses }, type: 'customNode' },
+    { id: '40', position: { x: 1850, y: 250 }, data: { label: 'EAE (CPSC Elective) (3)', realCourses }, type: 'customNode' },
+    { id: '41', position: { x: 1850, y: 350 }, data: { label: 'UCC (3)', realCourses }, type: 'customNode' },
+    { id: '42', position: { x: 1850, y: 450 }, data: { label: 'UCC (3)', realCourses }, type: 'customNode' },
+    { id: '43', position: { x: 1850, y: 550 }, data: { label: 'EAE (Emphasis Area Elective) (3)', realCourses }, type: 'customNode' },
   ];
   const edges: Edge[] = [
     {
@@ -129,8 +153,6 @@ const FlowChart = () => {
           width: 20,
           height: 20,
           color: '#020616',
-    
-
         },
     },
     {
@@ -227,22 +249,22 @@ const FlowChart = () => {
 
         },
     },
-    {
-      id: 'e7-25',  // Unique ID for the edge
-        source: '7', // Node 1 as the source
-        target: '25', // Node 2 as the target
-        sourceHandle: 'top', // Specify which handle on Node 1
-        targetHandle: 'top',  // Specify which handle on Node 2
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-          width: 20,
-          height: 20,
-          color: '#020616',
+    // {
+    //   id: 'e7-25',  // Unique ID for the edge
+    //     source: '7', // Node 1 as the source
+    //     target: '25', // Node 2 as the target
+    //     sourceHandle: 'top', // Specify which handle on Node 1
+    //     targetHandle: 'top',  // Specify which handle on Node 2
+    //     markerEnd: {
+    //       type: MarkerType.ArrowClosed,
+    //       width: 20,
+    //       height: 20,
+    //       color: '#020616',
     
 
-        },
-        type: 'step'
-    },
+    //     },
+    //     type: 'step'
+    // },
     {
       id: 'e7-13',  // Unique ID for the edge
         source: '7', // Node 1 as the source
